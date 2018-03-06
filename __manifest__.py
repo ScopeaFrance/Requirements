5f68b21b83ded976a1a7715afe2e408d16313f43
William Olhasque <william.olhasque@scopea.fr>
2018-03-06 15:57:46 +0100

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Dummy plugin',
+    'version': '10.0.1.0.0',
+    'category': 'Accounting & Finance',
+    'license': 'AGPL-3',
+    'summary': 'Dummy plugin',
+    'author': 'Scopea',
+    'website': 'https://www.scopea.fr',
+    'depends': [
+        'account',
+        'base_iban',
+        'base_business_document_import',
+        'onchange_helper',
+        ],
+    'data': [    ],
+    'images': [],
+    'installable': False,
+}
